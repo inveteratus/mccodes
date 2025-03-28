bcd1b6bb9cc8b3226ba1fb0279bb79345b1c4e97
Inveteratus <alan.mcfarlane@gmail.com>
2025-03-28 08:27:04 +0000

    Refactored login

--- public/login.php
+++ public/login.php
@@ -1,80 +1,82 @@
 <?php
 
-global $set;
-require __DIR__ . '/../include/globals_nonauth.php';
-$login_csrf = request_csrf_code('login');
-print
-        <<<EOF
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>{$set['game_name']}</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<script type="text/javascript" src="js/login.js"></script>
-<link href="css/login.css" type="text/css" rel="stylesheet" />
-</head>
-<body onload="getme();">
-<center>
-<table width="970" border="0" cellpadding="0" cellspacing="0" class="table2">
-<tr>
-<td class="lgrad"></td>
-<td class="center"><img src="title.jpg" alt="Mccodes Version 2" /><br />
-<!-- Begin Main Content -->
-EOF;
-$IP = str_replace(['/', '\\', '\0'], '', $_SERVER['REMOTE_ADDR']);
-if (file_exists('ipbans/' . $IP))
-{
-    die(
-            "<span style='font-weight: bold; color:red;'>
-            Your IP has been banned, there is no way around this.
-            </span></body></html>");
-}
-$year = date('Y');
-echo "<h3>&gt; {$set['game_name']} Log-In</h3>
-<table width='80%'>
-<tr>
-<td width='50%'>
-<fieldset>
-<legend>About {$set['game_name']}</legend>
-" . nl2br($set['game_description']) . '
-</fieldset>
-</td>
-<td>';
-echo <<<EOF
-<fieldset>
-<legend>Login</legend>
-<form action='authenticate.php' method='POST' name='login' onsubmit='return saveme();'>
-Username: <input type='text' name='username' /><br />
-Password: <input type='password' name='password' /><br />
-Remember me?<br />
-<input type='radio' value='ON' name='save' /> Yes
-<input type='radio' value='OFF' name='save' /> No<br />
-<input type='hidden' name='verf' value='{$login_csrf}' />
-<input type='submit' value='Submit'>
-</form>
-</fieldset>
-EOF;
-echo "</td></tr></table><br />
-<h3><a href='register.php'>REGISTER NOW!</a></h3><br />
-<i><center>Copyright &copy; {$year} {$set['game_owner']}</center></i>";
-print
-        <<<OUT
-</td>
-<td class="rgrad"></td>
-</tr>
-<tr>
-<td colspan="3">
-<table cellpadding="0" cellspacing="0" border="0" width="100%">
-<tr>
-<td class="dgradl">&nbsp;</td>
+require __DIR__ . '/../vendor/autoload.php';
 
-<td class="dgrad">&nbsp;</td>
-<td class="dgradr">&nbsp;</td>
-</tr>
-</table>
-</td>
-</tr>
-</table>
-</body>
-</html>
-OUT;
+use App\Classes\Database;
+
+session_start(['name' => 'MCCSID']);
+if (!isset($_SESSION['started'])) {
+    session_regenerate_id();
+    $_SESSION['started'] = microtime(true);
+}
+
+$email = $error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = array_key_exists('email', $_POST) && is_string($_POST['email']) ? trim($_POST['email']) : '';
+    $password = array_key_exists('password', $_POST) && is_string($_POST['password']) ? trim($_POST['password']) : '';
+
+    if (strlen($email) && strlen($password)) {
+        $env = Dotenv\Dotenv::createArrayBacked(dirname(__DIR__))->load();
+        $db = new Database("mysql:host={$env['MYSQL_HOST']};charset=utf8mb4;dbname={$env['MYSQL_DATABASE']}", $env['MYSQL_USER'], $env['MYSQL_PASSWORD']);
+        $user = $db->execute('SELECT userid AS id, userpass AS password, pass_salt AS salt FROM users WHERE email = :email', ['email' => $email])->fetch(PDO::FETCH_OBJ) ?: null;
+        if ($user && !strcmp($user->password, md5($user->salt . md5($password)))) {
+            $now = time();
+            $sql = <<<SQL
+                UPDATE users
+                SET lastip_login = :ip, last_login = :time
+                WHERE userid = :id
+            SQL;
+            $db->execute($sql, ['ip' => $_SERVER['REMOTE_ADDR'], 'time' => time(), 'id' => $user->id]);
+
+            $_SESSION['userid'] = (int)$user->id;
+            $_SESSION['loggedin'] = true;
+
+            session_regenerate_id();
+            session_write_close();
+
+            header('Location: /loggedin.php');
+            exit;
+        }
+        else {
+            $error = '<span class="text-red-500 text-sm">Invalid Credentials</span>';
+        }
+    }
+}
+
+$email = htmlentities($email);
+
+ob_start(fn ($buffer) => trim(preg_replace('/>\s+</', '><', $buffer)));
+
+echo <<<HTML
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <title>MCCodes</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body class="bg-slate-200 flex flex-col font-sans min-h-screen text-slate-700">
+        <main class="flex flex-col flex-grow items-center justify-center space-y-1">
+            <form action="/login.php" method="post" class="bg-slate-100 px-8 py-6 border border-slate-300 shadow-md rounded-md flex flex-col space-y-3 max-w-sm w-full">
+                <div class="flex flex-col space-y-0.5">
+                    <label for="email" class="text-sm text-slate-600 font-medium">Email</label>
+                    <input id="email" type="email" name="email" value="{$email}" autofocus autocomplete="email" required class="border border-slate-300 focus:outline-none focus:ring-1 focus:ring-offset-0 focus:border-blue-500 focus:ring-blue-500 p-2 bg-slate-50 w-full rounded" />
+                    {$error}
+                </div>
+                <div class="flex flex-col space-y-0.5">
+                    <label for="password" class="text-sm text-slate-600 font-medium">Password</label>
+                    <input id="password" type="password" name="password" autocomplete="current-password" required class="border border-slate-300 focus:outline-none focus:ring-1 focus:ring-offset-0 focus:border-blue-500 focus:ring-blue-500 p-2 bg-slate-50 w-full rounded" />
+                </div>
+                <div>
+                    <button type="submit" class="text-sm px-3 py-2 text-white bg-blue-500 focus:ring-2 focus:ring-offset-1 focus:ring-blue-500 focus:ring-offset-slate-50 focus:outline-none rounded font-medium">Login</button>
+                </div>
+            </form>
+            <p class="text-sm">
+                <a class="text-slate-600 hover:underline focus:underline focus:outline-none" href="/register.php">Not got an account yet ?</a>
+            </p>                             
+        </main>
+    </body>
+    </html>
+HTML;
---
+++ src/Classes/Database.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Classes;
+
+use PDO;
+use PDOStatement;
+
+class Database extends PDO
+{
+    public function __construct(string $dsn, string $user, string $password)
+    {
+        parent::__construct($dsn, $user, $password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+    }
+
+    public function execute(string $sql, array $context = []): PDOStatement
+    {
+        $statement = $this->prepare($sql);
+        $statement->execute($context);
+
+        return $statement;
+    }
+}
