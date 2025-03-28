04c55ac2ee420310f2d838343a29aadd37f20246
Inveteratus <alan.mcfarlane@gmail.com>
2025-03-28 11:36:07 +0000

    Cleanup of views

---
+++ views/layouts/guest.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <title>MCCodes</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body class="bg-slate-200 flex flex-col font-sans min-h-screen text-slate-700">
+        @yield('content')
+    </body>
+</html>
